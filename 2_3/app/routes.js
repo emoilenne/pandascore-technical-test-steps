631a112230d22f663f4451f6a4a9c5c1c39f3430
emoilenne <emoilenne@gmail.com>
2017-09-25 20:34:24 -0700

    Loaded data to the script

--- 2_3/app/routes.js
+++ 2_3/app/routes.js
@@ -6,13 +6,12 @@ module.exports = router;
 
 router.get('/', function(req, res) {
   var data = JSON.parse(require('fs').readFileSync('data.json', 'utf8'));
-  var champions = data['champions']
+  var champions = data['champions'];
   champions.sort(function (a, b) {
     a = a['name'].toLowerCase();
     b = b['name'].toLowerCase();
     return (a < b) ? -1 : (a > b) ? 1 : 0;
-  })
+  });
   res.locals.champions = champions;
-  res.locals.search_results = champions;
-  res.render('pages/index')
+  res.render('pages/index');
 });
