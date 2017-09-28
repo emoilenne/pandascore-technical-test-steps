dd95864e511c171e788610b05c95a57dbb7f16d3
emoilenne <emoilenne@gmail.com>
2017-09-28 16:03:52 -0700

    Displaying roles chart

--- 2_4/app/routes.js
+++ 2_4/app/routes.js
@@ -1,3 +1,37 @@
+function get_popularity_data(data) {
+  var popularity_dates = {};
+
+  for (var match_index = 0; match_index < data['matches'].length; match_index++) {
+    var match = data['matches'][match_index];
+    var date = Math.floor(match['timestamp'] / 864000).toString();
+    if (!(date in popularity_dates)) {
+      popularity_dates[date] = { 'total': 0};
+    }
+    if (!(match['champion'].toString() in popularity_dates[date])) {
+      popularity_dates[date][match['champion'].toString()] = 0;
+    }
+    popularity_dates[date][match['champion'].toString()]++;
+    popularity_dates[date]['total']++;
+  }
+  for (var date in popularity_dates) {
+    for (var champion_index = 0; champion_index < date.length; champion_index++) {
+      date[champion_index] /= date['total'] / 100;
+    }
+    delete date['total'];
+  }
+  // console.log(popularity_dates);
+  // restructure data so we can easily extract it for one champion
+  var popularity_champions = [];
+  for (var date in popularity_dates) {
+    for (var champion in popularity_dates[date]) {
+      popularity_champions.push({'champion': champion, 'date' : date});
+    }
+  }
+  return popularity_champions;
+}
+
+
+
 var express = require('express');
 var path =    require('path');
 
@@ -13,5 +47,7 @@ router.get('/', function(req, res) {
     return (a < b) ? -1 : (a > b) ? 1 : 0;
   });
   res.locals.champions = champions;
+  res.locals.matches = data['matches'];
+  res.locals.popularity_data = get_popularity_data(data);
   res.render('pages/index');
 });
---
+++ 2_4/public/scripts/search.js
@@ -0,0 +1,48 @@
+function search(champions_data) {
+  champions_data.sort(function (a, b) {
+    a = a['name'].toLowerCase();
+    b = b['name'].toLowerCase();
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+  })
+
+  searchBar = document.getElementById('searchBar');
+  var query = searchBar.value.trim().toLowerCase();
+
+  var champions = document.getElementsByClassName('champion');
+  var roles = document.getElementsByClassName('checkbox');
+
+  // should search if we have some query or any checkbox is on
+  var should_search = query
+  for (var role_index = 0; role_index < roles.length; role_index++) {
+    should_search = should_search || roles[role_index].checked;
+  }
+
+  if (!should_search) {
+    for (var champion_index = 0; champion_index < champions.length; champion_index++) {
+      champions[champion_index].style.display = "";
+    }
+    return;
+  }
+
+  // check query
+  for (var champion_index = 0; champion_index < champions.length; champion_index++) {
+    var name = champions[champion_index].getElementsByTagName('center')[0].innerHTML;
+
+    if (!query || name.toLowerCase().indexOf(query) != -1) {
+      champions[champion_index].style.display = "";
+    } else {
+      champions[champion_index].style.display = "none";
+    }
+  }
+
+  // check checkboxes
+  for (var role_index = 0; role_index < roles.length; role_index++) {
+    if (roles[role_index].checked) {
+      for (var champion_index = 0; champion_index < champions.length; champion_index++) {
+        if (!(champions_data[champion_index]['tags'].includes(roles[role_index].id))) {
+          champions[champion_index].style.display = "none";
+        }
+      }
+    }
+  }
+}
